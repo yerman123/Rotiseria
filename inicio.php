ce81f13b0c6c297f5b344dde1ddbe138bc605114
NicolasZGH <147265658+NicolasZGH@users.noreply.github.com>
2024-11-03 14:36:53 -0300

    Update inicio.php

--- inicio.php
+++ inicio.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION['username']) || empty($_SESSION['username'])) {
 }
 
 // Verificar el tiempo de inactividad (30 minutos)
-$inactive = 1800; // 30 minutos en segundos
+$inactive = 30000; // 30 minutos en segundos
 if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity'] > $inactive)) {
     // Si pasó más tiempo del permitido
     session_unset();     // Eliminar todas las variables de sesión
@@ -92,7 +92,7 @@ echo "<h2>Pedidos agrupados por clientes</h2>";
 if (count($pedidosPorCliente) > 0) {
     echo "<table class='tabla-pedidos' border='0'>";
     foreach ($pedidosPorCliente as $cliente => $pedidos) {
-        echo "<tr><th colspan='5'>Cliente: $cliente | Total Acumulado: $" . number_format($totalPorCliente[$cliente], 2) . "</th></tr>";
+        echo "<tr><th colspan='5' style='background-color:#473d38'>Cliente: $cliente | Total Acumulado: $" . number_format($totalPorCliente[$cliente], 2) . "</th></tr>";
         echo "<tr><th>Producto</th><th>Cantidad</th><th>Precio Total</th><th>Fecha de Pedido</th><th>Acciones</th></tr>";
         foreach ($pedidos as $pedido) {
             echo "<tr>";
@@ -119,14 +119,15 @@ if (count($pedidosPorCliente) > 0) {
     }
     echo "</table>";
 } else {
-    echo "No hay pedidos disponibles.";
+    echo "<h4>No hay pedidos disponibles. Agregue un pedido desde la página <a href='pedidos.php' style='color: #e4491a; font-weight: bold;'>agregar pedidos</a></h4>";
+
 }
 
 echo "<button class='faq-button'>
     <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 320 512'>
         <path d='M80 160c0-35.3 28.7-64 64-64h32c35.3 0 64 28.7 64 64v3.6c0 21.8-11.1 42.1-29.4 53.8l-42.2 27.1c-25.2 16.2-40.4 44.1-40.4 74V320c0 17.7 14.3 32 32 32s32-14.3 32-32v-1.4c0-8.2 4.2-15.8 11-20.2l42.2-27.1c36.6-23.6 58.8-64.1 58.8-107.7V160c0-70.7-57.3-128-128-128H144C73.3 32 16 89.3 16 160c0 17.7 14.3 32 32 32s32-14.3 32-32zm80 320a40 40 0 1 0 0-80 40 40 0 1 0 0 80z'></path>
     </svg>
-    <span class='tooltip'>Contactanos por ayuda al: (número)</span>
+    <span class='tooltip'>Contactanos por ayuda al número: 3644444444</span>
 </button>";
 
 echo "</div>"; 
